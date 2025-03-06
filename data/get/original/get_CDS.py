444d6394a5bd672836b2deb6cc6e4fa028b00db4
viea3104 <viea3104@usherbrooke.ca>
2025-03-05 21:41:38 -0500

    get_CDS is added, that file allow the user to download the CDS data from the source https://cds.climate.copernicus.eu/requests?tab=all using the API
    get_wildfire is added, that file allow the user to download the canada wildfire data on a google drive repository
    
    data_management contains all the preprocessing function and others used for the data analysis
    
    pixel_label is addedd and contains some function used for visualisation purposes
    
    plot_map is added and contains a function used for visualisation purpose
    
    data_preprocessing is added, that file allow to transform the dowloaded data into preprocessed data
    
    data_analysis is a notebook explaining what we choosed for our training dataset

---
+++ data/get/original/get_CDS.py
@@ -0,0 +1,58 @@
+import cdsapi
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+dataset_path = os.path.join(current_dir,'..', '..', '..', 'storage', 'dataset','weather.grib')
+dataset_path = os.path.abspath(dataset_path)
+
+dataset = "reanalysis-era5-single-levels"
+key = '684ab03f-dd8e-4c19-a669-b465462ef088'
+if key == 'your_key' :
+    print("You need to replace the variable \'key\' with your API token on the website: https://cds.climate.copernicus.eu")
+    sys.exit()
+
+request = {
+    "product_type": ["reanalysis"],
+    "variable": [
+        "10m_u_component_of_wind",
+        "10m_v_component_of_wind",
+        "2m_dewpoint_temperature",
+        "2m_temperature",
+        "surface_pressure",
+        "total_precipitation"
+    ],
+    "year": [
+        "2010", "2011", "2012",
+        "2013", "2014", "2015",
+        "2016", "2017", "2018",
+        "2019", "2020", "2021",
+        "2022", "2023", "2024",
+        "2025"
+    ],
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ],
+    "day": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12",
+        "13", "14", "15",
+        "16", "17", "18",
+        "19", "20", "21",
+        "22", "23", "24",
+        "25", "26", "27",
+        "28", "29", "30",
+        "31"
+    ],
+    "time": ["12:00"],
+    "data_format": "grib",
+    "download_format": "unarchived",
+    "area": [63, -80, 45, -57]
+}
+
+client = cdsapi.Client(url="https://cds.climate.copernicus.eu/api/",key = key)
+client.retrieve(dataset, request,str(dataset_path))
---
+++ data/prep/data_preprocessing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import cfgrib
+import os
+import sys
+from data.utils.data_management import *
+current_dir = os.path.dirname(os.path.abspath(__file__))
+dfweather_path = os.path.join(current_dir, '..', '..', 'storage', 'dataset','weather.grib')
+dfwildfire_path = os.path.join(current_dir, '..', '..', 'storage', 'dataset','CANADA_WILDFIRES.csv')
+generate_path = os.path.join(current_dir, '..', '..', 'storage', 'dataset',"dataset_pre_analysis.csv")
+generate_fire_path = os.path.join(current_dir, '..', '..', 'storage', 'dataset',"dataset_fire_cleaned.csv")
+dfweather_path  = os.path.abspath(dfweather_path)
+dfwildfire_path = os.path.abspath(dfwildfire_path)
+
+with cfgrib.open_dataset(dfweather_path) as ds:
+    df_weather = ds.to_dataframe().reset_index()
+
+
+df_weather.columns = df_weather.columns.str.upper()
+df_fire = pd.read_csv(dfwildfire_path)
+df_fire["REP_DATE"] = pd.to_datetime(df_fire["REP_DATE"])
+df_fire = df_fire[(df_fire["REP_DATE"] > "2009-12-31") & (df_fire["SRC_AGENCY"] == "QC")]
+df_fire = df_fire.drop(columns=["SRC_AGENCY"])
+df_weather["TIME"] = pd.to_datetime(df_weather["TIME"])
+df_weather["TIME"] = df_weather["TIME"].dt.date
+df_weather, df_fire ,incr_lat,incr_lon= add_coo_area(df_weather, df_fire)
+df_weather=add_rh_vpd(df_weather)
+df_fire = aggregate_fire(df_fire)
+df_fire = excess_fire_distribution(df_fire,incr_lat,incr_lon)
+df_fire = add_burned_density_isfire(df_fire)
+df_fire.to_csv(generate_fire_path, index=False)
+df_final = merge_fire_weather(df_fire,df_weather)
+df_final = df_final.drop(columns=['STEP','PROTZONE','ECOZ_NAME','CAUSE','FID','NUMBER','SURFACE'])
+df_final.to_csv(generate_path, index=False)
