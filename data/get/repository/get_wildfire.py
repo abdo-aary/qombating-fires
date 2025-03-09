d11db16c8023af04b18a7c463b066b6ca640dd60
viea3104 <viea3104@usherbrooke.ca>
2025-03-09 15:16:38 -0400

    get transformedd andd raw data change andd commenting notebook commenting code

--- data/get/repository/get_wildfire.py
+++ data/get/repository/get_wildfire.py
@@ -2,9 +2,9 @@ import gdown
 import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 extract_to = os.path.join(current_dir,'..', '..', '..', 'storage', 'dataset','wildfires')
-dataset_path = os.path.join(current_dir,'..', '..', '..', 'storage', 'dataset','wildfires','wildfires_data.csv')
+dataset_path = os.path.join(current_dir,'..', '..', '..', 'storage', 'dataset','wildfires','CANADA_WILDFIRES.csv')
 dataset_path = os.path.abspath(dataset_path)
 
-url = 'https://drive.google.com/uc?id=19ZqBzA1zCKM_YtOA16sy6rHTqw0RmVzz'
+url = "https://drive.google.com/uc?id=1kjIsv7JOjmLfK841b86NsrxnxavSD2T8"
 gdown.download(url, dataset_path, quiet=False)
 
