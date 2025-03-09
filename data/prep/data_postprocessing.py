dd3997aabeff2a0baefc502cfe6c77e6678679d6
viea3104 <viea3104@usherbrooke.ca>
2025-03-09 14:21:25 -0400

    get transformedd andd raw data change andd commenting notebook commenting code

--- data/prep/data_postprocessing.py
+++ data/prep/data_postprocessing.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-dfsample= os.path.join(current_dir, '..', '..', 'storage', 'output','test_samples_predictions.csv')
+dfsample= os.path.join(current_dir, '..', '..', 'storage', 'output','test_samples_predictions_embed_bassir.csv')
 dfsample_measure = os.path.join(current_dir, '..', '..', 'storage', 'output','test_samples_measure.csv')
 df = pd.read_csv(dfsample)
 df[["CELL_WIDTH","CELL_LENGTH"]] = vectorized_cell_width_length(df["CELL_LAT"],df["CELL_LON"],0.25,0.25)
